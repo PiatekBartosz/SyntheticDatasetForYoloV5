de55fd9e93875474186f055922b9e93ed06c0d95
PiatekBartosz <bartekpiatek123@gmail.com>
2023-02-19 20:14:36 +0100

    Added new classes to dataset

--- Machine learning dataset/rename.py
+++ Machine learning dataset/rename.py
@@ -2,14 +2,15 @@
 import os
 
 def main():
-    folder = 'white50'
-    for count, filename in enumerate(os.listdir(folder)):
-        src = f'{folder}/{filename}'
-        dst = f'{folder}/{str(count+1)}.jpg'
-        print(src, " ", dst)
-        if dst == src or not os.isdir(src):
-            continue
+    folders = ['white80circ','white80rect','black80circ','black80rect',]
+    for folder in folders:
+        for count, filename in enumerate(os.listdir(folder)):
+            src = f'{folder}/{filename}'
+            dst = f'{folder}/{str(count+1)}.jpg'
+            print(src, " ", dst)
+            if dst == src:
+                continue
 
-        os.rename(src, dst)
+            os.rename(src, dst)
 
 main()
